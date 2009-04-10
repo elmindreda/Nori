2bdcb9cc17f24ebc879384304ad1a0e55210c167
Camilla Berglund <elmindreda@elmindreda.org>
2009-04-10 16:38:38 +0000

    Added FBO support stub.

---
+++ include/wendy/GLFBO.h
@@ -0,0 +1,71 @@
+///////////////////////////////////////////////////////////////////////
+// Wendy OpenGL library
+// Copyright (c) 2009 Camilla Berglund <elmindreda@elmindreda.org>
+//
+// This software is provided 'as-is', without any express or implied
+// warranty. In no event will the authors be held liable for any
+// damages arising from the use of this software.
+//
+// Permission is granted to anyone to use this software for any
+// purpose, including commercial applications, and to alter it and
+// redistribute it freely, subject to the following restrictions:
+//
+//  1. The origin of this software must not be misrepresented; you
+//     must not claim that you wrote the original software. If you use
+//     this software in a product, an acknowledgment in the product
+//     documentation would be appreciated but is not required.
+//
+//  2. Altered source versions must be plainly marked as such, and
+//     must not be misrepresented as being the original software.
+//
+//  3. This notice may not be removed or altered from any source
+//     distribution.
+//
+///////////////////////////////////////////////////////////////////////
+#ifndef WENDY_GLFBO_H
+#define WENDY_GLFBO_H
+///////////////////////////////////////////////////////////////////////
+
+namespace wendy
+{
+  namespace GL
+  {
+  
+///////////////////////////////////////////////////////////////////////
+
+using namespace moira;
+
+///////////////////////////////////////////////////////////////////////
+
+class CanvasInfo
+{
+public:
+  unsigned int width;
+  unsigned int height;
+  unsigned int colorBits;
+  unsigned int depthBits;
+  unsigned int stencilBits;
+};
+
+///////////////////////////////////////////////////////////////////////
+
+class Framebuffer
+{
+public:
+  ~Framebuffer(void);
+  static Framebuffer* createInstance(Context& context);
+private:
+  Framebuffer(Context& context);
+  bool init(void);
+  Context& context;
+  unsigned int bufferID;
+};
+
+///////////////////////////////////////////////////////////////////////
+
+  } /*namespace GL*/
+} /*namespace wendy*/
+
+///////////////////////////////////////////////////////////////////////
+#endif /*WENDY_GLFBO_H*/
+///////////////////////////////////////////////////////////////////////
