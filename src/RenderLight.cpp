a0f4ae66c082c7a297186d006022d948856ae0ef
Camilla Berglund <elmindreda@elmindreda.org>
2010-04-26 19:55:43 +0000

    Inclusion work.

--- src/RenderLight.cpp
+++ src/RenderLight.cpp
@@ -25,14 +25,6 @@
 
 #include <wendy/Config.h>
 
-#include <wendy/Core.h>
-#include <wendy/Color.h>
-#include <wendy/Vector.h>
-#include <wendy/Rectangle.h>
-#include <wendy/Sphere.h>
-#include <wendy/Pixel.h>
-#include <wendy/Signal.h>
-
 #include <wendy/GLContext.h>
 
 #include <wendy/RenderLight.h>
