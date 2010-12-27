cfa158ebe812fe0b48e2b4a3feae555579bd3bfa
Camilla Berglund <elmindreda@elmindreda.org>
2010-12-27 14:51:56 +0000

    Formatting.

--- src/Frustum.cpp
+++ src/Frustum.cpp
@@ -55,9 +55,9 @@ Frustum::Frustum(float FOV, float aspectRatio, float farZ)
 
 bool Frustum::contains(const Vec3& point) const
 {
-  for (PlaneList::const_iterator i = planes.begin();  i != planes.end();  i++)
+  for (PlaneList::const_iterator p = planes.begin();  p != planes.end();  p++)
   {
-    if (!(*i).contains(point))
+    if (!p->contains(point))
       return false;
   }
 
@@ -66,9 +66,9 @@ bool Frustum::contains(const Vec3& point) const
 
 bool Frustum::contains(const Sphere& sphere) const
 {
-  for (PlaneList::const_iterator i = planes.begin();  i != planes.end();  i++)
+  for (PlaneList::const_iterator p = planes.begin();  p != planes.end();  p++)
   {
-    if (!(*i).contains(sphere))
+    if (!p->contains(sphere))
       return false;
   }
 
@@ -80,15 +80,13 @@ bool Frustum::contains(const AABB& box) const
   float minX, minY, minZ, maxX, maxY, maxZ;
   box.getBounds(minX, minY, minZ, maxX, maxY, maxZ);
 
-  for (PlaneList::const_iterator i = planes.begin();  i != planes.end();  i++)
+  for (PlaneList::const_iterator p = planes.begin();  p != planes.end();  p++)
   {
-    const Plane& plane = *i;
+    const Vec3 negative(p->normal.x < 0.f ? maxX : minX,
+                        p->normal.y < 0.f ? maxY : minY,
+                        p->normal.z < 0.f ? maxZ : minZ);
 
-    const Vec3 negative(plane.normal.x < 0.f ? maxX : minX,
-                        plane.normal.y < 0.f ? maxY : minY,
-                        plane.normal.z < 0.f ? maxZ : minZ);
-
-    if (!plane.contains(negative))
+    if (!p->contains(negative))
       return false;
   }
 
@@ -97,11 +95,9 @@ bool Frustum::contains(const AABB& box) const
 
 bool Frustum::intersects(const Sphere& sphere) const
 {
-  for (PlaneList::const_iterator i = planes.begin();  i != planes.end();  i++)
+  for (PlaneList::const_iterator p = planes.begin();  p != planes.end();  p++)
   {
-    const Plane& plane = *i;
-
-    if (plane.normal.dot(sphere.center) - sphere.radius > plane.distance)
+    if (p->normal.dot(sphere.center) - sphere.radius > p->distance)
       return false;
   }
 
@@ -113,15 +109,13 @@ bool Frustum::intersects(const AABB& box) const
   float minX, minY, minZ, maxX, maxY, maxZ;
   box.getBounds(minX, minY, minZ, maxX, maxY, maxZ);
 
-  for (PlaneList::const_iterator i = planes.begin();  i != planes.end();  i++)
+  for (PlaneList::const_iterator p = planes.begin();  p != planes.end();  p++)
   {
-    const Plane& plane = *i;
+    const Vec3 positive(p->normal.x < 0.f ? minX : maxX,
+                        p->normal.y < 0.f ? minY : maxY,
+                        p->normal.z < 0.f ? minZ : maxZ);
 
-    const Vec3 positive(plane.normal.x < 0.f ? minX : maxX,
-                        plane.normal.y < 0.f ? minY : maxY,
-                        plane.normal.z < 0.f ? minZ : maxZ);
-
-    if (!plane.contains(positive))
+    if (!p->contains(positive))
       return false;
   }
 
@@ -130,13 +124,14 @@ bool Frustum::intersects(const AABB& box) const
 
 void Frustum::transformBy(const Transform3& transform)
 {
-  for (PlaneList::iterator i = planes.begin();  i != planes.end();  i++)
-    (*i).transformBy(transform);
+  for (PlaneList::iterator p = planes.begin();  p != planes.end();  p++)
+    p->transformBy(transform);
 }
 
 void Frustum::set(float FOV, float aspectRatio, float farZ)
 {
   planes.clear();
+  planes.reserve(5);
 
   const float radians = FOV * (float) M_PI / 180.f;
   const float distance = 0.5f / tanf(radians / 2.f);
