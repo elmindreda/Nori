91314e96013ef873e7e427042e8a94b570eb1a74
Camilla Berglund <elmindreda@elmindreda.org>
2011-03-17 03:59:21 +0000

    Removed some garbage.

--- media/cube.fs
+++ media/cube.fs
@@ -1,23 +1,22 @@
 
-uniform sampler2D checkers;
+uniform vec3 color;
 
 const vec3 L = normalize(vec3(1, 0, 1));
 
 varying vec3 viewpos;
 varying vec3 normal;
-varying vec2 mapping;
 
 void main()
 {
-  vec3 C = vec3(1.0, 0.3, 0.3) * texture2D(checkers, mapping).r;
+  vec3 C = color;
 
   vec3 R = reflect(L, normal);
   vec3 V = viewpos;
 
   float Ia = 0.1;
-  float Id = clamp(dot(normal, L), 0, 1);
-  float Is = pow(clamp(dot(R, V), 0, 1), 15);
+  float Id = clamp(dot(normal, L), 0.0, 1.0);
+  float Is = pow(clamp(dot(R, V), 0.0, 1.0), 15.0);
 
-  gl_FragColor = vec4(C * (Ia + Id) + vec3(1) * Is, 1);
+  gl_FragColor = vec4(C * (Ia + Id) + Is, 1.0);
 }
 
--- media/cube.vs
+++ media/cube.vs
@@ -12,21 +12,11 @@ in Vertex vertex;
 
 varying vec3 viewpos;
 varying vec3 normal;
-varying vec2 mapping;
 
 void main()
 {
-  vec3 viewpos = (MV * vec4(vertex.position, 1)).xyz;
-
-  vec3 u = normalize(viewpos);
-  vec3 n = (MV * vec4(vertex.normal, 0)).xyz;
-  vec3 r = reflect(u, n);
-  vec3 t = r + vec3(0, 0, 1);
-  float p = sqrt(dot(t, t));
-
-  viewpos = normalize(viewpos);
-  normal = n;
-  mapping = vec2(0.5 + r.x / (2 * p), 0.5 + r.y / (2 * p));
+  viewpos = (MV * vec4(vertex.position, 1)).xyz;
+  normal = vertex.normal;
 
   gl_Position = MVP * vec4(vertex.position, 1);
 }
