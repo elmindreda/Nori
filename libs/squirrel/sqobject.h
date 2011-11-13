d19fdeb2fa47ebc707301c2ba7ce4123ab8d2485
Camilla Berglund <elmindreda@elmindreda.org>
2011-11-13 16:49:35 +0100

    Fixed warning on GCC.

--- libs/squirrel/sqobject.h
+++ libs/squirrel/sqobject.h
@@ -1,4 +1,8 @@
 /*	see copyright notice in squirrel.h */
+/* 2011-11-13  Camilla Berglund  <elmindreda@elmindreda.org>
+ *   Fixed warning on GCC.
+ */
+
 #ifndef _SQOBJECT_H_
 #define _SQOBJECT_H_
 
@@ -291,7 +295,7 @@ struct SQObjectPtr : public SQObject
 	{
 		__Release(_type ,_unVal);
 		_type = OT_NULL;
-		_unVal.raw = NULL;
+		_unVal.raw = SQRawObjectVal(0);
 	}
 	private:
 		SQObjectPtr(const SQChar *){} //safety
