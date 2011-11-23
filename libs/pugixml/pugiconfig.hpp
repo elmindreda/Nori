4e5ed9962168048aada5acedf78e0e5ff37c8558
Camilla Berglund <elmindreda@elmindreda.org>
2011-11-23 18:01:59 +0100

    Disabled pugixml XPath and exception support.

--- libs/pugixml/pugiconfig.hpp
+++ libs/pugixml/pugiconfig.hpp
@@ -18,7 +18,7 @@
 // #define PUGIXML_WCHAR_MODE
 
 // Uncomment this to disable XPath
-// #define PUGIXML_NO_XPATH
+#define PUGIXML_NO_XPATH
 
 // Uncomment this to disable STL
 // Note: you can't use XPath with PUGIXML_NO_STL
@@ -26,7 +26,7 @@
 
 // Uncomment this to disable exceptions
 // Note: you can't use XPath with PUGIXML_NO_EXCEPTIONS
-// #define PUGIXML_NO_EXCEPTIONS
+#define PUGIXML_NO_EXCEPTIONS
 
 // Set this to control attributes for public classes/functions, i.e.:
 // #define PUGIXML_API __declspec(dllexport) // to export all public symbols from DLL
